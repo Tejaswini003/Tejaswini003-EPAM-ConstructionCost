c20703e5fa680c62061d50c5fedb6900333d22ee
S.V.Subbarao.P <S.V.Subbarao.P@192.168.0.4>
2020-07-21 19:09:09 +0530

    ConstructionCost

---
+++ constructioncost/src/test/java/com/code/constructioncost/TestConstructionCost.java
@@ -0,0 +1,19 @@
+package com.code.constructioncost;
+import org.junit.*;
+import static org.junit.Assert.*;
+public class TestConstructionCost 
+{
+
+	 @Test
+		public void test() 
+	   {
+
+			ConstructionCost cc = new ConstructionCost();
+
+			double answer = cc.costForConstructionMaterial("Standard", 500, false);
+
+			assertEquals(600000,answer,0.001);
+
+		}
+}
+
